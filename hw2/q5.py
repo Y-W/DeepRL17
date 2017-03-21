ab20fdfff40e020c1679826f7cd4bc164392163d
Aldo Pareja <aldo.pareja@live.com>
2017-03-20 22:21:49 -0400

    added q5.py and deeplearner

---
+++ hw2/q5.py
@@ -0,0 +1,44 @@
+import logging
+import numpy as np
+import tensorflow as tf
+
+from game_replay_parallel import GameEngine_Train
+from util import ExponentialDecay, Constant
+from q_learner import DeepLearner, LinearLeaner, SimpleQ
+from framework import Train
+
+
+FLAGS = tf.app.flags.FLAGS
+
+tf.app.flags.DEFINE_integer('batch', 32, 'Batch size')
+tf.app.flags.DEFINE_integer('total_updates', 100000, 'Number of training updates')
+tf.app.flags.DEFINE_float('learning_rate_initial', 1e-2, 'Initial learning rate')
+tf.app.flags.DEFINE_float('learning_rate_final', 1e-2, 'Final learning rate')
+tf.app.flags.DEFINE_float('weight_decay', 1e-4, 'Weight Decay')
+tf.app.flags.DEFINE_float('decay', 0.99, 'Decay factor')
+tf.app.flags.DEFINE_integer('light_eval', 100, 'Number of light evals')
+tf.app.flags.DEFINE_integer('full_eval', 10, 'Number of full evals')
+tf.app.flags.DEFINE_string('output_dir', 'outputs/q5', 'Output directory')
+
+def main(argv=None):
+    logging.disable(logging.INFO)
+
+    game_engine = GameEngine_Train(FLAGS.batch, 10)
+
+    learning_rate = ExponentialDecay(FLAGS.learning_rate_initial, FLAGS.learning_rate_final, FLAGS.total_updates)
+    game_info = game_engine.game_batch
+    learner = DeepLearner(game_info.state_shape, game_info.action_n, FLAGS.batch, learning_rate)
+    q = SimpleQ(learner)
+
+    updates_per_full_eval = FLAGS.total_updates // FLAGS.full_eval
+    updates_per_light_eval = FLAGS.total_updates // FLAGS.light_eval
+    train = Train(q, game_engine, FLAGS.decay,
+                  1, 1, updates_per_light_eval, 
+                  updates_per_full_eval, updates_per_full_eval, 
+                  FLAGS.total_updates, FLAGS.output_dir,
+                  True)
+    
+    train()
+
+if __name__ == '__main__':
+    tf.app.run()
--- hw2/q_learner.py
+++ hw2/q_learner.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+import tensorflow as tf
 
 
 #TODO gradient clipping - huber loss
@@ -183,3 +184,107 @@ class LinearLeaner(Learner):
         tmp = np.load(filePath)
         self.W = tmp['W']
         self.B = tmp['B']
+
+class DeepLearner(Learner):
+    def __init__(self, state_shape, action_n, batch_size, learning_rate):
+        self.state_shape = state_shape
+        self.action_n = action_n
+        self.batch_size = batch_size
+        self.learning_rate = learning_rate
+        #TODO set up the neural network just as in the paper
+        with tf.Graph().as_default():
+            with tf.name_scope('pre_proc'):
+                self.input = tf.placeholder(tf.float32, 
+                                            shape = (batch_size,) + state_shape,
+                                            name = 'input')
+            with tf.name_scope('conv_layers'):
+                self.conv1 = tf.layers.conv2d(inputs = self.input,
+                                              filters = 32,
+                                              kernel_size= (8,8),
+                                              strides = (4,4),
+                                              data_format = 'channels_first',
+                                              kernel_initializer = tf.contrib.layers.xavier_initializer(),
+                                              bias_initializer = tf.constant_initializer(0.1),
+                                              activation = tf.nn.relu,
+                                              name = 'conv1')
+                self.conv2 = tf.layers.conv2d(inputs = self.conv1,
+                                              filters = 64,
+                                              kernel_size= (4,4),
+                                              strides = (2,2),
+                                              data_format = 'channels_first',
+                                              kernel_initializer = tf.contrib.layers.xavier_initializer(),
+                                              bias_initializer = tf.constant_initializer(0.1),
+                                              activation = tf.nn.relu,
+                                              name = 'conv2')
+                self.conv3 = tf.layers.conv2d(inputs = self.conv2,
+                                              filters = 64,
+                                              kernel_size= (3,3),
+                                              strides = (1,1),
+                                              data_format = 'channels_first',
+                                              kernel_initializer = tf.contrib.layers.xavier_initializer(),
+                                              bias_initializer = tf.constant_initializer(0.1),
+                                              activation = tf.nn.relu,
+                                              name = 'conv3')
+            with tf.name_scope('FCLs'):
+                self.flat = tf.reshape(self.conv3,(32,-1),name='flat') 
+                self.fcn1 = tf.layers.dense(inputs = self.flat,
+                                            units = 512,
+                                            activation = tf.nn.relu,
+                                            kernel_initializer = tf.contrib.layers.xavier_initializer(),
+                                            name = 'fcn1')
+                self.fcn2 = tf.layers.dense(inputs = self.fcn1,
+                                            units = action_n,
+                                            activation = None,
+                                            kernel_initializer = tf.contrib.layers.xavier_initializer(),
+                                            name = 'linear')
+            with tf.name_scope('Loss'):
+                self.actions = tf.placeholder(tf.int32,
+                                              shape = batch_size,
+                                              name = 'actions')
+                self.act_one_hot = tf.one_hot(indices = self.actions,
+                                              depth = action_n,
+                                              on_value = 1.0,
+                                              off_value = 0.0,
+                                              name = 'act_one_hot')
+                self.q_pred = tf.reduce_sum(self.fcn2 * self.act_one_hot,
+                                            reduction_indices = 1,
+                                            name = 'q_pred')
+                self.targets = tf.placeholder(tf.float32,
+                                              shape = batch_size,
+                                              name = 'targets')
+                self.err = tf.abs(self.q_pred - self.targets)
+                self.hub_loss = tf.where(condition = self.err<1.0,
+                                         x = 0.5 * tf.square(self.err),
+                                         y = self.err - 0.5,
+                                         name = 'clipped')
+            with tf.name_scope('optim'):
+                self.mean_loss = tf.reduce_mean(self.hub_loss,
+                                                name = 'mean_loss')
+                self.train = tf.train.AdamOptimizer(learning_rate=1e-4, 
+                                                    epsilon = 1e-3).minimize(self.mean_loss)
+            self.sess = tf.Session()
+            self.sum_loss = tf.summary.scalar('loss',tf.reduce_mean(self.err))
+            self.sum_writer = tf.summary.FileWriter('~/TensorBoard/',self.sess.graph)
+            self.init = tf.global_variables_initializer()
+            self.sess.run(self.init)
+            self.saver = tf.train.Saver()
+
+    def eval_batch(self, input_batch):
+        #returns the forward pass of the input_batch
+        return self.sess.run(self.fcn2,
+                             feed_dict = {self.input:input_batch})
+
+    def update_batch(self, input_batch, action_batch, target_batch):
+        #modifies the weights according with y
+        _, sum_loss = self.sess.run([self.train,self.sum_loss],
+                      feed_dict = {self.input:input_batch,
+                                   self.actions:action_batch,
+                                   self.targets:target_batch})
+        self.sum_writer.add_summary(sum_loss)
+        self.sum_writer.flush()
+    
+    def save(self, filePath):
+        self.saver.save(self.sess,filePath + 'model')
+    
+    def load(self, filePath):
+        self.saver.restore(self.sess,filePath + 'model')
